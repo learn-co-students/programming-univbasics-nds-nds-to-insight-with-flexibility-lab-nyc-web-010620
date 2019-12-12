8d68477b3cc9d11e13237e64914239c8ec121ac2
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-12-12 21:12:02 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
-require 'pry'
+
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -98,7 +98,7 @@ def movies_with_directors_set(source)
     movies = ele[:movies]
     new_array << movies_with_director_key(name, movies)
 
-    #binding.pry
+    
 
 
   end
